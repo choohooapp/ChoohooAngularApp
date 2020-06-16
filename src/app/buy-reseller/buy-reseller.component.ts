49cfc25fbe0aac92b04726fbef541294211a10f6
waynemaponga <leewaynemaponga@gmail.com>
2020-06-16 22:56:17 +0200

    added payfast

--- src/app/buy-reseller/buy-reseller.component.ts
+++ src/app/buy-reseller/buy-reseller.component.ts
@@ -2,6 +2,8 @@ import { Component,OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../api.service';
 import { IfStmt } from '@angular/compiler';
+import {map} from 'rxjs/internal/operators';
+
 
 @Component({
   selector: 'app-buy-reseller',
@@ -11,9 +13,25 @@ import { IfStmt } from '@angular/compiler';
 export class BuyResellerComponent implements OnInit, OnDestroy {
   id: any;
   private sub: any;
-  errorMessage:any
-  details = {ResellerID: '', Name: '', AdminStatus: '',Type:"", units:""};
-  constructor(private route: ActivatedRoute ,public Api :ApiService,) {}
+  errorMessage:any;
+   UnitPurchase:number;
+   UnitPurchaseTotal:number;
+    showBuycredits:boolean;
+    showCreditsReview:boolean;
+    DateTimeNow : any;
+    VatAmount: number;
+    TotalAmount:number;
+    PaymentMethod:string;
+  details = {ResellerID: '', Name: '', AdminStatus: '',Type:"", units:"" , Country:'' , City:'' , Email:'' , Cellphone : ''};
+  constructor(private route: ActivatedRoute ,public Api :ApiService,) {
+    this.UnitPurchase = 0.00;
+    this.UnitPurchaseTotal = this.UnitPurchase * 50;
+    this.showBuycredits = true;
+    this.DateTimeNow = new Date();
+    this.VatAmount = 150;
+    this.TotalAmount = this.UnitPurchaseTotal = this.UnitPurchase * 50;
+    this.PaymentMethod = 'mc';
+  }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
@@ -50,16 +68,17 @@ export class BuyResellerComponent implements OnInit, OnDestroy {
         this.errorMessage=" No Reseller Profile";
         }
   
-  
-  
         if (response) {
-  
-     
-  
+
       this.details.ResellerID = response.person.ResellerID;
       this.details.Name = response.person.Name;
       this.details.AdminStatus = response.person.ApplicationStatus;
       this.details.units = response.person.units;
+      this.details.Country = response.person.Country
+      this.details.City = response.person.City;
+      this.details.Email = response.person.Email;
+      this.details.Cellphone = response.person.Mobilenumber;
+
       if(this.details.AdminStatus ==="0"){
         this.details.AdminStatus = "Pending"
         //this.buybtn = false
@@ -89,4 +108,48 @@ export class BuyResellerComponent implements OnInit, OnDestroy {
   
       
     }
+
+    IncreaseUnitPurchase()
+    {
+      this.UnitPurchase ++;
+      this.UnitPurchaseTotal = this.UnitPurchase * 50;
+        this.TotalAmount = this.UnitPurchaseTotal ;
+    }
+
+    DecreaseUnitPurchase(){
+      this.UnitPurchase --;
+      this.UnitPurchaseTotal = this.UnitPurchase * 50;
+        this.TotalAmount = this.UnitPurchaseTotal ;
+    }
+    gotoReview()
+    {
+      if(this.UnitPurchase < 1)
+      {
+        return;
+      }
+      this.showCreditsReview = true;
+      this.showBuycredits = false;
+    }
+    gotoPayment()
+    {
+
+        const Units = this.UnitPurchase * 50.00;
+        const data = {
+            name_first: this.details.Name,
+            name_last: this.details.ResellerID,
+            email_address: this.details.Email,
+            cell_number: this.details.Cellphone,
+            item_name: Units +  ' Units',
+            item_description: Units +  ' Units',
+            amount: this.TotalAmount,
+            m_payment_id: this.details.ResellerID,
+            payment_method:this.PaymentMethod,
+        }
+        const myJSON = encodeURIComponent(JSON.stringify(data));
+        console.log(data);
+        window.open('http://royallez.co.za/test/checkout.php?data=' + myJSON, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+
+
+    }
+
 }
